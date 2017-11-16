371b926e6e06235cd99e478fd338e2ba06279a11
Fannar Magnússon <fannar@kth.se>
2017-11-16 11:14:22 +0100

    random split dataset to training and testing datasets 80/20

--- lab1/src/main/scala/se/kth/spark/lab1/task3/Main.scala
+++ lab1/src/main/scala/se/kth/spark/lab1/task3/Main.scala
@@ -1,8 +1,7 @@
 package se.kth.spark.lab1.task3
 
 import org.apache.spark._
-import org.apache.spark.sql.{SQLContext, DataFrame}
-import org.apache.spark.ml.tuning.CrossValidatorModel
+import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.ml.regression.LinearRegressionModel
 import org.apache.spark.ml.PipelineModel
 
@@ -13,9 +12,23 @@ object Main {
     val sqlContext = new SQLContext(sc)
 
     import sqlContext.implicits._
-    import sqlContext._
 
     val filePath = "src/main/resources/millionsong.txt"
+
+    // first we need to read the data into RDD to be able to split the file 80/20 (training/testing)
+    val RDD = sc.textFile(filePath)
+    println("Total data set length: " + RDD.count())
+    val splitRDDs = RDD.randomSplit(Array(0.8, 0.2), seed = 23)
+
+    val trainingRDD = splitRDDs(0) // 80% of the dataset
+    val trainingDF = trainingRDD.toDF()
+
+    val testingRDD = splitRDDs(1)  // 20% of the dataset
+    val testingDF = testingRDD.toDF()
+
+    println("Training set length: " + trainingDF.count())
+    printf("Testing set length: " + testingDF.count())
+
     val obsDF: DataFrame = ???
 
     val myLR = ???
