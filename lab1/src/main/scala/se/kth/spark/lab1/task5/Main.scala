be0322490b7319569e360a450e5492c9bf591433
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-11-16 16:48:11 +0100

    task 5 work

--- lab1/src/main/scala/se/kth/spark/lab1/task5/Main.scala
+++ lab1/src/main/scala/se/kth/spark/lab1/task5/Main.scala
@@ -1,30 +1,57 @@
 package se.kth.spark.lab1.task5
 
 import org.apache.spark._
-import org.apache.spark.sql.{ SQLContext, DataFrame }
+import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.ml.tuning.CrossValidatorModel
-import org.apache.spark.ml.regression.LinearRegressionModel
-import org.apache.spark.ml.PipelineModel
+import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.feature.PolynomialExpansion
+import se.kth.spark.lab1.task2
 
 object Main {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("lab1").setMaster("local")
-    val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
+    //val conf = new SparkConf().setAppName("lab1").setMaster("local")
+    //val sc = new SparkContext(conf)
+    //val sqlContext = new SQLContext(sc)
 
-    import sqlContext.implicits._
-    import sqlContext._
+    // Let's call task 2 as a function
+    // Get the pipeline stages defined there, as well as the spark and SQL contexts
+    val (sc, sqlContext, task2PipelineStages) = task2.Main.main(Array())
 
-    val filePath = "src/main/resources/millionsong.txt"
-    val obsDF: DataFrame = ???
+    //import sqlContext.implicits._
+    //import sqlContext._
 
-    val myLR = ???
-    val lrStage = ???
-    val pipeline = ???
-    val pipelineModel: PipelineModel = ???
+    val filePath = "src/main/resources/millionsong-500k-noquotes.txt"
+    val obsDF: DataFrame = sqlContext.read.text(filePath)
+
+    // Create polynomial expansion transformer
+    val polynomialExpansionTransformer = new PolynomialExpansion().setInputCol("features").setOutputCol("polyfeatures").setDegree(2)
+
+    // Create linear regression transformer to pipeline (duplicated from task 3)
+    val myLR = new LinearRegression()
+      .setLabelCol("label")
+      .setFeaturesCol("polyfeatures")
+      .setMaxIter(10)
+      .setRegParam(0.1)
+      .setElasticNetParam(0.1)
+
+    // Add transformers to pipeline stages array
+    val pipelineStages = task2PipelineStages ++ Array(polynomialExpansionTransformer, myLR)
+
+    val pipeline = new Pipeline().setStages(pipelineStages)
+    val pipelineModel: PipelineModel = pipeline.fit(obsDF)
+
+    val lrStage = pipelineStages.length - 1
     val lrModel = pipelineModel.stages(lrStage).asInstanceOf[LinearRegressionModel]
 
-    //print rmse of our model
+    val lrTrainingSummary = lrModel.summary
+    println("LinearRegressionTrainingSummary:")
+    println(s"RMSE: ${lrTrainingSummary.rootMeanSquaredError}")
+    println(s"numIterations: ${lrTrainingSummary.totalIterations}")
+
     //do prediction - print first k
+    val modelProcessedDF = pipelineModel.transform(obsDF)
+    println("Task 3 predictions - modelProcessedDF:")
+    modelProcessedDF.show(10)
   }
 }
