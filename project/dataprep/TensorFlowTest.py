acd173540e72fe81574f3d0cdf6faa497ca3f290
Fannar Magnússon <fannar@kth.se>
2017-12-12 11:37:54 +0100

    extract genre from image name

--- project/dataprep/TensorFlowTest.py
+++ project/dataprep/TensorFlowTest.py
@@ -3,13 +3,16 @@ from PIL import Image  # from Pillow
 
 # Just a small test file to play around with tensorflow locally on our dataset
 
-img = Image.open("spectrograms/Metal_Cobalt_Gin.png")
-print(img.format, img.size, img.mode)
+im = Image.open("spectrograms/Metal_Cobalt_Gin.png")
+# im.filename is spectrogram/file.png - extract the image filename
+filename = im.filename.split("/")[len(im.filename.split("/")) - 1]
+genre = filename.split("_")[0]
+print(genre, im.size)
 # img.show()
 
-image_contents = tf.read_file("spectrograms/Cobalt_Gin_Metal.png")
-decoded_image = tf.image.decode_png(image_contents, channels=1, dtype=tf.uint8)
-print(decoded_image.shape)
+#image_contents = tf.read_file("spectrograms/Cobalt_Gin_Metal.png")
+#decoded_image = tf.image.decode_png(image_contents, channels=1, dtype=tf.uint8)
+#print(decoded_image.shape)
 #resized_image = tf.image.resize_images(decoded_image, [129, 129])
 #print(resized_image.shape)
 #print(resized_image)
