779ccaa5b37e57bfa931116dc20ad688d1be47ea
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-12-16 17:35:08 +0100

    genre classifier WIP

--- project/genreclassifier/GenreClassifier.py
+++ project/genreclassifier/GenreClassifier.py
@@ -3,12 +3,13 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import glob
 import random
+import sys
 
 
 def input_parser(img_path, label_value):
-    label = tf.one_hot(label_value, NUM_CLASSES)
+    label = tf.one_hot(label_value, n_classes)
     img_file = tf.read_file(img_path)
-    img_decoded = tf.image.decode_image(img_file, channels=1)
+    img_decoded = tf.cast(tf.image.decode_image(img_file, channels=1), tf.float32)
     return img_decoded, label
 
 
@@ -29,7 +30,42 @@ def load_and_shuffle_data():
     random.shuffle(data)
     image_paths = [x[0] for x in data]
     labels = [x[1] for x in data]
-    return tf.constant(image_paths), tf.constant(labels)
+    return image_paths, labels
+
+
+def conv_net(x, n_classes, dropout, reuse, is_training):
+    # Define a scope for reusing the variables
+    with tf.variable_scope('ConvNet', reuse=reuse):
+        # MNIST data input is a 1-D vector of 784 features (28*28 pixels)
+        # Reshape to match picture format [Height x Width x Channel]
+        # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
+        x = tf.reshape(x, shape=[-1, 128, 128, 1])
+
+        # Convolution Layer with 32 filters and a kernel size of 5
+        conv1 = tf.layers.conv2d(x, 32, 5, activation=tf.nn.relu)
+        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+        conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
+
+        # Convolution Layer with 32 filters and a kernel size of 5
+        conv2 = tf.layers.conv2d(conv1, 64, 3, activation=tf.nn.relu)
+        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+        conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
+
+        # Flatten the data to a 1-D vector for the fully connected layer
+        fc1 = tf.contrib.layers.flatten(conv2)
+
+        # Fully connected layer (in contrib folder for now)
+        fc1 = tf.layers.dense(fc1, 1024)
+        # Apply Dropout (if is_training is False, dropout is not applied)
+        fc1 = tf.layers.dropout(fc1, rate=dropout, training=is_training)
+
+        # Output layer, class prediction
+        out = tf.layers.dense(fc1, n_classes)
+        # Because 'softmax_cross_entropy_with_logits' already apply softmax,
+        # we only apply softmax to testing network
+        out = tf.nn.softmax(out) if not is_training else out
+
+    return out
 
 
 # Validate our data, plot first element
@@ -50,6 +86,8 @@ def validate_data(data_set):
 
 if __name__ == "__main__":
 
+    sess = tf.Session()
+
     # Our music genre labels
     label_dict = {
         'Classical': 0,
@@ -59,7 +97,16 @@ if __name__ == "__main__":
         'Metal': 4,
         'Rock': 5
     }
-    NUM_CLASSES = len(label_dict)
+
+    n_classes = len(label_dict)
+    learning_rate = 0.001
+    num_steps = 4 # Size of data set
+    batch_size = 128
+    display_step = 100
+    # Network Parameters
+    #n_input = 784  # MNIST data input (img shape: 28*28)
+    #n_classes = 10  # MNIST total classes (0-9 digits)
+    dropout = 0.75  # Dropout, probability to keep units
 
     images, labels = load_and_shuffle_data()
 
@@ -67,7 +114,84 @@ if __name__ == "__main__":
     tf_data_set = tf.data.Dataset.from_tensor_slices((images, labels))
     tf_data_set = tf_data_set.map(input_parser)
 
-    validate_data(tf_data_set)
+    # validate_data(tf_data_set)
+
+    # Set up batching
+    batch_data_set = tf_data_set.batch(1)
+    #iterator = batch_data_set.make_one_shot_iterator()
+    #iterator = batch_data_set.make_initializable_iterator()
+    # Reinitializable iterator
+    iterator = tf.data.Iterator.from_structure(batch_data_set.output_types, batch_data_set.output_shapes)
+    training_iterator_init_op = iterator.make_initializer(batch_data_set)
+
+    #_data = tf.placeholder(tf.float32, [None, 128, 128, 1])
+    #_labels = tf.placeholder(tf.float32, [None, n_classes])
+    # Initialize the iterator
+    #sess.run(iterator.initializer, feed_dict={_data: images, _labels: labels})
+
+    # images, labels
+    X, Y = iterator.get_next()
+
+    # Verify batching
+    # with tf.Session() as sess:
+    #print (sess.run(Y))
+
+    # Create a graph for training
+    logits_train = conv_net(X, n_classes, dropout, reuse=False, is_training=True)
+    # Create another graph for testing that reuse the same weights, but has
+    # different behavior for 'dropout' (not applied).
+    logits_test = conv_net(X, n_classes, dropout, reuse=True, is_training=False)
+
+    # Define loss and optimizer (with train logits, for dropout to take effect)
+    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits_train, labels=Y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = optimizer.minimize(loss_op)
+
+    # Evaluate model (with test logits, for dropout to be disabled)
+    correct_pred = tf.equal(tf.argmax(logits_test, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    # Initialize the variables (i.e. assign their default value)
+    init = tf.global_variables_initializer()
+
+    # Run the initializer
+    sess.run(init)
+
+    # Training cycle
+    sess.run(training_iterator_init_op)
+    for step in range(1, num_steps + 1):
+
+        # # NOTE: For initializable iterator
+        # try:
+        #     # Run optimization
+        #     sess.run(train_op)
+        # except tf.errors.OutOfRangeError:
+        #     # Reload the iterator when it reaches the end of the dataset
+        #     sess.run(iterator.initializer, feed_dict={_data: images, _labels: labels})
+        #     sess.run(train_op)
+
+        try:
+            sess.run(train_op)
+            print("Running train op")
+        except tf.errors.OutOfRangeError:
+            print("Reached end of data set in train op")
+            sess.run(training_iterator_init_op)
+
+        if True: #step % display_step == 0 or step == 1:
+            # Calculate batch loss and accuracy
+            # (note that this consume a new batch of data)
+            try:
+                loss, acc = sess.run([loss_op, accuracy])
+                print("Running loss op")
+                print("Step " + str(step) + ", Minibatch Loss= " + \
+                      "{:.4f}".format(loss) + ", Training Accuracy= " + \
+                      "{:.3f}".format(acc))
+            except tf.errors.OutOfRangeError:
+                print("Reached end of data set on loss step - done")
+                sess.run(training_iterator_init_op)
+
+    print("Optimization Finished!")
+
 
 
 
