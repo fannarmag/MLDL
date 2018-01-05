79b62a03524338442eb539f3cd10872c95a5bbb0
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2018-01-05 21:32:28 +0100

    pkeep variable in inferencer

--- project/genreclassifier/Inferencer.py
+++ project/genreclassifier/Inferencer.py
@@ -29,7 +29,7 @@ def image_to_array(img_path):
 if __name__ == '__main__':
     # Let's allow the user to pass the filename as an argument
     parser = argparse.ArgumentParser()
-    parser.add_argument("--frozen_model_filename", default="trained_model/frozen_model.pb",
+    parser.add_argument("--frozen_model_filename", default="trained_model_3/frozen_model.pb",
                         type=str, help="Frozen model file to import")
     args = parser.parse_args()
 
@@ -48,14 +48,16 @@ if __name__ == '__main__':
     x = graph.get_tensor_by_name('prefix/Reshape:0')
     # y = graph.get_tensor_by_name('prefix/Accuracy/predictions:0')
     y = graph.get_tensor_by_name('prefix/prediction:0')
+    pkeep = graph.get_tensor_by_name('prefix/pkeep:0')
+
 
     # We launch a Session
     with tf.Session(graph=graph) as sess:
         # Note: we don't nee to initialize/restore anything
         # There is no Variables in this graph, only hardcoded constants
         y_out = sess.run(y, feed_dict={
-            #x: [[3, 5, 7, 4, 5, 1, 1, 1, 1, 1]]  # < 45
-            x: image_to_array("data/testing/Techno/Techno_0_60.png")
+            x: image_to_array("data/testing/Pop/Pop_1_4.png"),
+            pkeep: 1
         })
         # I taught a neural net to recognise when a sum of numbers is bigger than 45
         # it should return False in this case
